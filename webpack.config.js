e3d67b0cf9c579e2890e91ce9c02c93fdf05fcc8
Ryan Clay <clay.Christopher29@yahoo.com>
2019-09-13 14:26:26 -0400

    conflicts fixed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const WebpackShellPlugin = require("webpack-shell-plugin");
 module.exports = {
-<<<<<<< HEAD
     entry: "./src/scripts/main.js",
     devServer: {
         writeToDisk: true
@@ -37,41 +36,4 @@ module.exports = {
         filename: "bundle.js"
     },
     devtool: "eval-source-map"
-=======
-  entry: "./src/scripts/main.js",
-  devServer: {
-    writeToDisk: true
-  },
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader"
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
-  plugins: [
-    new WebpackShellPlugin({
-      onBuildEnd: ["json-server -p 8088 -w api/db.json"]
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    }),
-    new CopyPlugin([{ from: "./src/styles", to: "./styles" }])
-  ],
-  output: {
-    filename: "bundle.js"
-  },
-  devtool: "eval-source-map"
->>>>>>> master
 };
